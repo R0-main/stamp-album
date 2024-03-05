24833fa292150f570a337d491294388c29ada41b
Romain <justromixx@gmail.com>
2024-03-05 15:25:50 +0100

    copy button for each titled textField

--- src/components/text-input/text-input.tsx
+++ src/components/text-input/text-input.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../button/button";
+import { copyToClipboard } from "../../utils/copy-to-clipboard";
 
 function TextInput({
   icon,
@@ -15,16 +17,21 @@ function TextInput({
   width?: string;
   label?: string;
   defaultValue?: string;
-  onChange?: (value: string) => void;
+  onChange: (value: string) => void;
 }) {
+  const [text, setText] = useState(defaultValue || "");
+
   return (
     <>
       {label && (
-        <div className={`label w-[${width || "55rem"}]`}>
-          <span className="label-text text-primary text-2xl font-bold">
-            {label}
-          </span>
-        </div>
+        <>
+          <div className={`label w-[${width || "55rem"}]`}>
+            <span className="label-text text-primary text-2xl font-bold">
+              {label}
+            </span>
+            <Button onClick={() => copyToClipboard(text)}>Copier</Button>
+          </div>
+        </>
       )}
 
       <label
@@ -33,7 +40,10 @@ function TextInput({
         } items-center gap-2 border-2 w-[${width || "55rem"}]`}
       >
         <input
-          onChange={(e) => (onChange ? onChange(e.target.value) : null)}
+          onChange={(e) => {
+            onChange(e.target.value);
+            setText(e.target.value);
+          }}
           type={type || "text"}
           defaultValue={defaultValue}
           className=" placeholder:font-bold placeholder:text-primary placeholder:text-xl  text-primary  font-bold text-xl w-full"
